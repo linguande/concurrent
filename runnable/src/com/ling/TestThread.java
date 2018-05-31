46e38f3b4426c6cb32bc509d1151ba3205aaf16e
linguande <13570920879@163.com>
2018-05-31 15:49:46 +0800

    runable

---
+++ runnable/src/com/ling/TestThread.java
@@ -0,0 +1,9 @@
+package com.ling;
+
+/**
+ * @description: demo
+ * @author: linguande
+ * @create: 2018-05-31 15:46
+ **/
+public class TestThread {
+}
---
+++ runnable/src/com/ling/entity/RunnableDemo.java
@@ -0,0 +1,16 @@
+package com.ling.entity;
+
+/**
+ * @description: 通过实现Runnable创建线程
+ * @author: linguande
+ * @create: 2018-05-31 15:47
+ **/
+public class RunnableDemo implements Runnable {
+
+    private String threadName;
+
+    @Override
+    public void run() {
+
+    }
+}
