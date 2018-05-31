d616b1426ee76c2013f082bf5de4a316c243e3eb
linguande <13570920879@163.com>
2018-05-31 16:20:05 +0800

    runable

--- runnable/src/com/ling/TestThread.java
+++ runnable/src/com/ling/TestThread.java
@@ -1,9 +1,18 @@
 package com.ling;
 
+import com.ling.entity.RunnableDemo;
+
 /**
  * @description: demo
  * @author: linguande
  * @create: 2018-05-31 15:46
  **/
 public class TestThread {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new RunnableDemo("Thread-1"));
+        thread1.start();
+
+        Thread thread2 = new Thread(new RunnableDemo("Thread-2"));
+        thread2.start();
+    }
 }
--- runnable/src/com/ling/entity/RunnableDemo.java
+++ runnable/src/com/ling/entity/RunnableDemo.java
@@ -1,7 +1,7 @@
 package com.ling.entity;
 
 /**
- * @description: 通过实现Runnable创建线程
+ * @description: 实现Runnable接口
  * @author: linguande
  * @create: 2018-05-31 15:47
  **/
@@ -9,8 +9,26 @@ public class RunnableDemo implements Runnable {
 
     private String threadName;
 
+    public RunnableDemo() {
+
+    }
+
+    public RunnableDemo(String threadName) {
+        this.threadName = threadName;
+        System.out.println("Createing " + threadName);
+    }
+
     @Override
     public void run() {
-
+        System.out.println("Running " + threadName);
+        try {
+            for (int i = 4; i > 0; i--) {
+                System.out.println("Thread : " + threadName + "," + i);
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + " interrupted.");
+        }
+        System.out.println("Thread " + threadName + " exiting.");
     }
 }
